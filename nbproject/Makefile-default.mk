4de9f6e0bd3b9cb00bc53cf229882725df97f638
Lashmarr <156346074+Lashmarr@users.noreply.github.com>
2024-03-08 16:12:32 +0000

    added new file - calibrations

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../lab-6-motors-and-pwm-tomas-thomas.X/dc_motor.c ../lab-6-motors-and-pwm-tomas-thomas.X/main.c color.c ../lab-6-motors-and-pwm-tomas-thomas.X/calibration_routine.c
+SOURCEFILES_QUOTED_IF_SPACED=../lab-6-motors-and-pwm-tomas-thomas.X/dc_motor.c ../lab-6-motors-and-pwm-tomas-thomas.X/main.c color.c ../lab-6-motors-and-pwm-tomas-thomas.X/calibration.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1626605405/dc_motor.p1 ${OBJECTDIR}/_ext/1626605405/main.p1 ${OBJECTDIR}/color.p1 ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1626605405/dc_motor.p1.d ${OBJECTDIR}/_ext/1626605405/main.p1.d ${OBJECTDIR}/color.p1.d ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1626605405/dc_motor.p1 ${OBJECTDIR}/_ext/1626605405/main.p1 ${OBJECTDIR}/color.p1 ${OBJECTDIR}/_ext/1626605405/calibration.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1626605405/dc_motor.p1.d ${OBJECTDIR}/_ext/1626605405/main.p1.d ${OBJECTDIR}/color.p1.d ${OBJECTDIR}/_ext/1626605405/calibration.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1626605405/dc_motor.p1 ${OBJECTDIR}/_ext/1626605405/main.p1 ${OBJECTDIR}/color.p1 ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1
+OBJECTFILES=${OBJECTDIR}/_ext/1626605405/dc_motor.p1 ${OBJECTDIR}/_ext/1626605405/main.p1 ${OBJECTDIR}/color.p1 ${OBJECTDIR}/_ext/1626605405/calibration.p1
 
 # Source Files
-SOURCEFILES=../lab-6-motors-and-pwm-tomas-thomas.X/dc_motor.c ../lab-6-motors-and-pwm-tomas-thomas.X/main.c color.c ../lab-6-motors-and-pwm-tomas-thomas.X/calibration_routine.c
+SOURCEFILES=../lab-6-motors-and-pwm-tomas-thomas.X/dc_motor.c ../lab-6-motors-and-pwm-tomas-thomas.X/main.c color.c ../lab-6-motors-and-pwm-tomas-thomas.X/calibration.c
 
 
 
@@ -112,13 +112,13 @@ ${OBJECTDIR}/color.p1: color.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/color.d ${OBJECTDIR}/color.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/color.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/_ext/1626605405/calibration_routine.p1: ../lab-6-motors-and-pwm-tomas-thomas.X/calibration_routine.c  nbproject/Makefile-${CND_CONF}.mk 
+${OBJECTDIR}/_ext/1626605405/calibration.p1: ../lab-6-motors-and-pwm-tomas-thomas.X/calibration.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}/_ext/1626605405" 
-	@${RM} ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1 ../lab-6-motors-and-pwm-tomas-thomas.X/calibration_routine.c 
-	@-${MV} ${OBJECTDIR}/_ext/1626605405/calibration_routine.d ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/_ext/1626605405/calibration.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/1626605405/calibration.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/1626605405/calibration.p1 ../lab-6-motors-and-pwm-tomas-thomas.X/calibration.c 
+	@-${MV} ${OBJECTDIR}/_ext/1626605405/calibration.d ${OBJECTDIR}/_ext/1626605405/calibration.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/1626605405/calibration.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
 ${OBJECTDIR}/_ext/1626605405/dc_motor.p1: ../lab-6-motors-and-pwm-tomas-thomas.X/dc_motor.c  nbproject/Makefile-${CND_CONF}.mk 
@@ -145,13 +145,13 @@ ${OBJECTDIR}/color.p1: color.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/color.d ${OBJECTDIR}/color.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/color.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/_ext/1626605405/calibration_routine.p1: ../lab-6-motors-and-pwm-tomas-thomas.X/calibration_routine.c  nbproject/Makefile-${CND_CONF}.mk 
+${OBJECTDIR}/_ext/1626605405/calibration.p1: ../lab-6-motors-and-pwm-tomas-thomas.X/calibration.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}/_ext/1626605405" 
-	@${RM} ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1 ../lab-6-motors-and-pwm-tomas-thomas.X/calibration_routine.c 
-	@-${MV} ${OBJECTDIR}/_ext/1626605405/calibration_routine.d ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1626605405/calibration_routine.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/_ext/1626605405/calibration.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/1626605405/calibration.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/1626605405/calibration.p1 ../lab-6-motors-and-pwm-tomas-thomas.X/calibration.c 
+	@-${MV} ${OBJECTDIR}/_ext/1626605405/calibration.d ${OBJECTDIR}/_ext/1626605405/calibration.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/1626605405/calibration.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
