1e2169409341f3a650860355d0a9d7e9c3eb92fc
tmy21 <156363919+tmy21@users.noreply.github.com>
2024-03-11 02:08:53 +0000

    RGBC channels color read complete

--- color.c
+++ color.c
@@ -18,6 +18,23 @@ void color_click_init(void)
 	color_writetoaddr(0x01, 0xD5);
 }
 
+void color_TRILED_ON(void) {
+    //Turn on the tri-color LED (red, green, and blue) on color-clicker, to have white LED shining looking for card surfaces in maze
+    //Must be used after color_click_init() is called
+    
+    //set all LEDs to be outputs
+    TRISGbits.TRISG1 = 0; //Set TRIS value for red LED to zero (output)
+    TRISAbits.TRISA4 = 0; //Set TRIS value for green LED to zero (output)
+    TRISFbits.TRISF7 = 0; //Set TRIS value for blue LED to zero (output)
+            
+    //Turn on red, green and blue LEDs in tri-color LED
+    LATGbits.LATG1 = 1; //Set red LED on
+    LATAbits.LATA4 = 1; //Set green LED on
+    LATFbits.LATF7 = 1; //Set blue LED on
+    
+    //what other lights should we have on? Ask TOM!
+}
+
 void color_writetoaddr(char address, char value){
     I2C_2_Master_Start();         //Start condition
     I2C_2_Master_Write(0x52 | 0x00);     //7 bit device address + Write mode
@@ -26,7 +43,8 @@ void color_writetoaddr(char address, char value){
     I2C_2_Master_Stop();          //Stop condition
 }
 
-unsigned int color_read_Red(void)
+
+unsigned int color_read_Red(void) //this function reads the red light value from the color sensor (as a 16bit integer)
 {
 	unsigned int tmp;
 	I2C_2_Master_Start();         //Start condition
@@ -34,9 +52,51 @@ unsigned int color_read_Red(void)
 	I2C_2_Master_Write(0xA0 | 0x16);    //command (auto-increment protocol transaction) + start at RED low register
 	I2C_2_Master_RepStart();			// start a repeated transmission
 	I2C_2_Master_Write(0x52 | 0x01);     //7 bit address + Read (1) mode
-	tmp=I2C_2_Master_Read(1);			//read the Red LSB
-	tmp=tmp | (I2C_2_Master_Read(0)<<8); //read the Red MSB (don't acknowledge as this is the last read)
+	tmp=I2C_2_Master_Read(1);			//read the Red LSB ((1) means acknowledge that it has been read here)
+	tmp=tmp | (I2C_2_Master_Read(0)<<8); //read the Red MSB (don't acknowledge as this is the last read, i.e. (0) in function to set ack=0)
 	I2C_2_Master_Stop();          //Stop condition
 	return tmp;
 }
 
+unsigned int color_read_Green(void) //same function as above but for green light
+{
+	unsigned int tmp;
+	I2C_2_Master_Start();         //Start condition
+	I2C_2_Master_Write(0x52 | 0x00);     //7 bit address + Write mode
+	I2C_2_Master_Write(0xA0 | 0x18);    //command (auto-increment protocol transaction) + start at Green low register
+	I2C_2_Master_RepStart();			// start a repeated transmission
+	I2C_2_Master_Write(0x52 | 0x01);     //7 bit address + Read (1) mode
+	tmp=I2C_2_Master_Read(1);			//read the Green LSB
+	tmp=tmp | (I2C_2_Master_Read(0)<<8); //read the Green MSB (don't acknowledge as this is the last read)
+	I2C_2_Master_Stop();          //Stop condition
+	return tmp;
+}
+
+unsigned int color_read_Blue(void) //same function as above but for blue light
+{
+	unsigned int tmp;
+	I2C_2_Master_Start();         //Start condition
+	I2C_2_Master_Write(0x52 | 0x00);     //7 bit address + Write mode
+	I2C_2_Master_Write(0xA0 | 0x1A);    //command (auto-increment protocol transaction) + start at Blue low register
+	I2C_2_Master_RepStart();			// start a repeated transmission
+	I2C_2_Master_Write(0x52 | 0x01);     //7 bit address + Read (1) mode
+	tmp=I2C_2_Master_Read(1);			//read the Blue LSB
+	tmp=tmp | (I2C_2_Master_Read(0)<<8); //read the Blue MSB (don't acknowledge as this is the last read)
+	I2C_2_Master_Stop();          //Stop condition
+	return tmp;
+}
+
+unsigned int color_read_Clear(void) //same function as above but examining the clear channel on the color sensor
+                                    //can use this value to normalise the others (r,g,b) and to interpret brightness of reflected light, indicating when the buggy is near/approaching a colored card
+{
+	unsigned int tmp;
+	I2C_2_Master_Start();         //Start condition
+	I2C_2_Master_Write(0x52 | 0x00);     //7 bit address + Write mode
+	I2C_2_Master_Write(0xA0 | 0x14);    //command (auto-increment protocol transaction) + start at Clear low register
+	I2C_2_Master_RepStart();			// start a repeated transmission
+	I2C_2_Master_Write(0x52 | 0x01);     //7 bit address + Read (1) mode
+	tmp=I2C_2_Master_Read(1);			//read the Clear LSB
+	tmp=tmp | (I2C_2_Master_Read(0)<<8); //read the Clear MSB (don't acknowledge as this is the last read)
+	I2C_2_Master_Stop();          //Stop condition
+	return tmp;
+}
--- color.h
+++ color.h
@@ -16,12 +16,22 @@ void color_click_init(void);
  *  address is the register within the colour click to write to
  *	value is the value that will be written to that address
  ***********************************************/
+
+void color_TRILED_ON(void);
+//This function turns on all three LEDs (r,g,b) in the tri-color LED on the color-clicker
+//Produces white light which is used for detecting cards and their color
+
+
+
 void color_writetoaddr(char address, char value);
 
 /********************************************//**
- *  Function to read the red channel
+ *  Functions to read the red, green, blue, and clear channels
  *	Returns a 16 bit ADC value representing colour intensity
  ***********************************************/
 unsigned int color_read_Red(void);
+unsigned int color_read_Green(void);
+unsigned int color_read_Blue(void);
+unsigned int color_read_Clear(void);
 
 #endif
