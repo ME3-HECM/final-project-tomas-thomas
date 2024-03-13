dfffc4cc6167ea5ee8c9437921b607cb2929e576
Lashmarr <156346074+Lashmarr@users.noreply.github.com>
2024-03-13 21:35:03 +0000

    Fixed a major referencing issue
    Files were referenced to lab 6, now have created new files that are located int the final-project-tomas-thomas folder. Now files are referenced in the project which is how it should have been from the start.

---
+++ build/default/production/dc_motor_v1.p1.d
@@ -0,0 +1,3 @@
+build/default/production/dc_motor_v1.p1:  \
+dc_motor_v1.c  \
+dc_motor_v1.h 
---
+++ calibration.h
@@ -0,0 +1,26 @@
+#ifndef _calibration_H
+#define _calibration_H
+
+#include <xc.h>
+
+#define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
+
+typedef struct calibration_structure { //definition of DC_motor structure
+    char index;
+    char over;
+    char left_90;
+    char right_90;
+    char left_135;
+    char right_135;
+    char forward;
+    char forward_motorL;
+    char forward_motorR;
+} calibration_structure;
+
+struct calibration_structure calibration;
+
+void adjust_calibration(int *calibration_label);
+void switch_calibration(int *calibration_index);
+
+
+#endif
